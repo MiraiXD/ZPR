7b2d31daeeda0bfd8302db37e93c35b87db0623f
Ja-Kuba <jjk.kow@gmail.com>
2018-12-03 00:52:23 +0100

    8=>

--- test.cpp
+++ test.cpp
@@ -1,7 +1,7 @@
 //#define BOOST_TEST_MAIN
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE Hello
-#include <boost/test/unit_test.hpp>
+#include <boost/test/included/unit_test.hpp>
 /*#include "pch.h"
 #include <iostream>
 #include <fstream>
